7c24a9272182cb1517a294977ed29a88fc51fc7d
Brian Caruso <bdc34@brian-x230.(none)>
2013-03-21 18:05:21 -0400

    fixing deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,5 +3,5 @@
 # Make links from home configuration git repository files
 # to the places the are expected in the home directory.
 
-ln -f -s $HOME/homeconfig/dot.emacs.el $HOME/.emacs
-ln -f -s $HOME/homeconfig/dot.emacs-site-lisp $HOME/.emacs-site-lisp
+ln -f -s $HOME/homeConfig/dot.emacs.el $HOME/.emacs
+ln -f -s $HOME/homeConfig/dot.emacs-site-lisp $HOME/.emacs-site-lisp
