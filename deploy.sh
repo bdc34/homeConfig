8440bf7b84c6bcd49b63155f765b1c76ac80ed07
Brian Caruso <bdc34@cornell.edu>
2013-11-21 14:27:36 -0500

    Adding bashrc

--- deploy.sh
+++ deploy.sh
@@ -6,6 +6,7 @@
 ln -f -s $HOME/homeConfig/dot.emacs.el $HOME/.emacs
 ln -f -s $HOME/homeConfig/basic.el $HOME/.basic.el
 ln -f -s $HOME/homeConfig/dot.emacs-site-lisp $HOME/.emacs-site-lisp
+ln -f -s $HOME/homeConfig/bashrc $HOME/.bashrc
 
 mkdir -p $HOME/bin
 for FILE in $( ls $HOME/homeConfig/bin )
