5b9376fcc033368f6a0ea82925ac18bcff1a5b96
Brian Caruso <bdc34@cornell.edu>
2013-12-05 10:58:57 -0500

    Adding check for arxiv_opt/perl/bin and add it to PATH

--- dot.emacs.el
+++ dot.emacs.el
@@ -43,12 +43,15 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(ansi-color-names-vector ["#fdf6e3" "#dc322f" "#859900" "#b58900" "#268bd2" "#d33682" "#2aa198" "#657b83"])
- '(ansi-term-color-vector ["#586e75" "#dc322f" "#859900" "#b58900" "#268bd2" "#d33682" "#2aa198" "#002b36"])
+ '(ansi-term-color-vector ["#586e75" "#dc322f" "#859900" "#b58900" "#268bd2" "#d33682" "#2aa198" "#002b36"] t)
  '(custom-safe-themes (quote ("f5e56ac232ff858afb08294fc3a519652ce8a165272e3c65165c42d6fe0262a0" "d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" default)))
  '(dabbrev-case-fold-search nil)
  '(ecb-layout-window-sizes nil)
  '(ecb-options-version "2.40")
+ '(eclim-eclipse-dirs (quote ("/opt/eclipse4.3-kepler")))
+ '(eclim-executable "/opt/eclipse4.3-kepler/eclim")
  '(fci-rule-color "#073642")
+ '(follow-mouse-auto-raise-frame nil)
  '(global-highline-mode t)
  '(highlight-changes-colors (quote ("#d33682" "#6c71c4")))
  '(highlight-tail-colors (quote (("#073642" . 0) ("#546E00" . 20) ("#00736F" . 30) ("#00629D" . 50) ("#7B6000" . 60) ("#8B2C02" . 70) ("#93115C" . 85) ("#073642" . 100))))
