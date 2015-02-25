e08d6d7fce9ee8c7c7c37ce2e6a508b23774e4f4
Brian Caruso <bdc34@cornell.edu>
2015-02-25 12:33:27 -0500

    using melpa stable

--- dot.emacs-site-lisp/emacs24.el
+++ dot.emacs-site-lisp/emacs24.el
@@ -5,7 +5,7 @@
 (setq package-archives
              '(("gnu" . "http://elpa.gnu.org/packages/")
                ("marmalade" . "http://marmalade-repo.org/packages/")
-               ("melpa" . "http://melpa.milkbox.net/packages/")))
+               ("melpa-stable" . "http://stable.melpa.org/packages/")))
 (package-initialize)
 
 
