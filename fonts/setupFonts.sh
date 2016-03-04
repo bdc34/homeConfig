423774093e58f1c4f43225215aa53f6e32f23b14
Brian D. Caruso <bdc34@cornell.edu>
2016-03-04 16:48:55 -0500

    Adding fonts and fonts.conf

--- fonts/setupFonts.sh
+++ fonts/setupFonts.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/bash
 
-mkdir -f ~/.fonts
+if [ ! -e ~/.fonts ] 
+then
+	mkdir ~/.fonts
+fi
 cp ~/homeConfig/fonts/*.ttf ~/.fonts
+cp ~/homeConfig/fonts/fonts.conf ~/.fonts.conf
+unzip ~/homeConfig/fonts/*.zip -d ~/.fonts/
 fc-cache ~/.fonts
