273b996b1dfe0d8112ead8300ab79f38304222ee
Brian D. Caruso <bdc34@cornell.edu>
2016-05-05 11:21:11 -0400

    Changing from melpa to melpa-stable

--- dot.emacs-site-lisp/emacs24.el
+++ dot.emacs-site-lisp/emacs24.el
@@ -4,7 +4,10 @@
 (setq package-archives
              '(("gnu" . "http://elpa.gnu.org/packages/")
                ("marmalade" . "http://marmalade-repo.org/packages/")
-               ("melpa" . "http://melpa.org/packages/")))
+               ("melpa-stable" . "https://stable.melpa.org/packages/")
+;;               ("melpa" . "http://melpa.org/packages/")
+               ))
+
 (package-initialize)
 
 (defvar my-packages-list
