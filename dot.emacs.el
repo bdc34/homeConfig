c1d661eab659cf06451a38079abe59bfa9d6a257
Brian D. Caruso <bdc34@cornell.edu>
2019-12-17 14:33:24 -0500

    Change to web-mode comments

--- dot.emacs.el
+++ dot.emacs.el
@@ -140,6 +140,14 @@
      (360 . "#DC8CC3"))))
  '(vc-annotate-very-old-color "#DC8CC3")
  '(vc-follow-symlinks t)
+ '(web-mode-comment-formats
+   (quote
+    (("java" . "/*")
+     ("javascript" . "//")
+     ("php" . "/*"))) t)
+ '(web-mode-enable-auto-quoting nil)
+ '(web-mode-enable-css-colorization t)
+ '(web-mode-enable-current-element-highlight t)
  '(weechat-color-list
    (quote
     (unspecified "#002b36" "#073642" "#990A1B" "#dc322f" "#546E00" "#859900" "#7B6000" "#b58900" "#00629D" "#268bd2" "#93115C" "#d33682" "#00736F" "#2aa198" "#839496" "#657b83")))
